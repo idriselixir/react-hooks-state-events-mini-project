4bf690a2a82a70d4a0d2413ca6d650872b8f1c58
idris <idreesnoor150@gmail.com>
2023-04-23 14:57:36 +0300

    done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,14 @@
-import React from "react";
-
-function Task() {
+import React from 'react'
+function Task({ task, deleteTask }) {
   return (
-    <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
-    </div>
-  );
-}
 
-export default Task;
+    <div className='task'>
+      <div className='label'>{task.category}</div>
+      <div className='text'>{task.text}</div>
+      <button className='delete' onClick={deleteTask}>
+        X
+      </button>
+    </div>
+      )
+    }
+    export default Task
